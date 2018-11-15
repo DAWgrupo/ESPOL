794531370cd77b580a0ab435bd1d080e991f5665
Jaminson Riascos <jami.r.m@hotmail.com>
2018-11-14 23:37:01 -0500

    adding api support for credit card selection [#159610252]

--- pagosEnLineaApp/src/pages/seleccion-metodo-pago/seleccion-metodo-pago.ts
+++ pagosEnLineaApp/src/pages/seleccion-metodo-pago/seleccion-metodo-pago.ts
@@ -15,22 +15,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SeleccionMetodoPagoPage {
   selectedItem: any;
-  cards: any[];
+  cards: any[] = [];
   items: Array<{title: string, note: string, icon: string}>;
+  response: any = 
+  {
+    "cards": [
+        {
+            "bin": "511915",
+            "status": "review",
+            "token": "17121538682542236138",
+            "holder_name": "citlali calderon",
+            "expiry_year": "2020",
+            "expiry_month": "9",
+            "transaction_reference": "CI-473",
+            "type": "vi",
+            "number": "7991"
+        },
+        {
+            "bin": "422023",
+            "status": "valid",
+            "token": "15363681013452573066",
+            "holder_name": "citlali calderon",
+            "expiry_year": "2020",
+            "expiry_month": "9",
+            "transaction_reference": null,
+            "type": "mc",
+            "number": "8431"
+        },
+        {
+            "bin": "453254",
+            "status": "valid",
+            "token": "10135134879450157925",
+            "holder_name": "citlali calderon",
+            "expiry_year": "2020",
+            "expiry_month": "9",
+            "transaction_reference": null,
+            "type": "vi",
+            "number": "8311"
+        }
+    ],
+    "result_size": 3
+};
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
-    this.cards = [
-      {"number" : "xxxx xxxx xxxx 7991", "holder_name" : "Juniver Jair Roman Macias", "expiry_year" : "2021", "expiry_month": "9", "type": "mc"}, 
-      {"number" : "xxxx xxxx xxxx 0011", "holder_name" : "Juniver Jair Roman Macias", "expiry_year" : "2025" ,  "expiry_month": "8", "type": "vi"}
-    ];
+    this.getCards();
+    
 
     this.items = [];
   }
-
+  getCards(){
+    for (let card of this.response.cards) {
+      let tmp_card = {
+        "number" : card.bin.slice( 1, 5) + " XXXX XXXX " + card.number,
+        "holder_name" : card.holder,
+        "expiry_year" : card.expiry_year,
+        "expiry_month": card.expiry_month,
+        "type": card.type
+      };
+      this.cards.push(tmp_card)
+    }
+  }
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
     this.navCtrl.push(SeleccionMetodoPagoPage, {
