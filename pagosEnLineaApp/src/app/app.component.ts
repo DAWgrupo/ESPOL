03c2db3e9e60a19ae98e5ed5d884900b0f6fac24
unknown <jjroman@espol.edu.ec>
2018-11-26 20:00:20 -0500

    #159610327 #159610380 Carrito de Compras

--- pagosEnLineaApp/src/app/app.component.ts
+++ pagosEnLineaApp/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import {SeleccionMetodoPagoPage} from '../pages/seleccion-metodo-pago/seleccion-metodo-pago';
+import { CarritoPage } from '../pages/carrito/carrito';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -24,7 +26,8 @@ export class MyApp {
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'tarjetas', component: SeleccionMetodoPagoPage }
+      { title: 'tarjetas', component: SeleccionMetodoPagoPage },
+      { title: 'Carrito', component: CarritoPage }
     ];
 
   }
