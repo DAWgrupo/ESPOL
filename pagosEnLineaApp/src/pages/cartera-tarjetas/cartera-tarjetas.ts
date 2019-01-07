afae6320501c64ffea2de348c235ac68d0a2045c
Jaminson Riascos <jami.r.m@hotmail.com>
2019-01-07 03:05:44 -0500

    decoupled integration with pizzeria

--- pagosEnLineaApp/src/pages/cartera-tarjetas/cartera-tarjetas.ts
+++ pagosEnLineaApp/src/pages/cartera-tarjetas/cartera-tarjetas.ts
@@ -3,11 +3,9 @@ import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-an
 import { ApiServiceProvider } from '../../providers/api-service/api-service';
 import { Storage } from '@ionic/storage';
 import {  AlertController } from 'ionic-angular';
+import { Constantes } from '../../util/constantes';
 
 
-//declare var PaymentezForm;
-//declare var Paymentez;
-//declare var jQuery;
 
 /**
  * Generated class for the CarteraTarjetasPage page.
@@ -25,47 +23,14 @@ export class CarteraTarjetasPage {
   
   selectedItem: any;
   cards: Array< {holder_name: String, expiry_year: String, expiry_month: String, icon: String, number: String}> = [];
-  response: any = 
-  {
-    "cards": [
-        {
-            "bin": "511915",
-            "status": "review",
-            "token": "17121538682542236138",
-            "holder_name": "citlali calderon",
-            "expiry_year": "2020",
-            "expiry_month": "9",
-            "transaction_reference": "CI-473",
-            "type": "vi",
-            "number": "7991"
-        },
-        {
-            "bin": "422023",
-            "status": "valid",
-            "token": "15363681013452573066",
-            "holder_name": "citlali calderon",
-            "expiry_year": "2020",
-            "expiry_month": "9",
-            "transaction_reference": null,
-            "type": "mc",
-            "number": "8431"
-        },
-        {
-            "bin": "453254",
-            "status": "valid",
-            "token": "10135134879450157925",
-            "holder_name": "citlali calderon",
-            "expiry_year": "2020",
-            "expiry_month": "9",
-            "transaction_reference": null,
-            "type": "vi",
-            "number": "8311"
-        }
-    ],
-    "result_size": 3
-};
+  
 
-  constructor(public navCtrl: NavController, public storage: Storage, public navParams: NavParams, private api: ApiServiceProvider, public loadingCtrl: LoadingController,  public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController,
+     public storage: Storage,
+      public navParams: NavParams,
+       private api: ApiServiceProvider,
+        public loadingCtrl: LoadingController,
+          public alertCtrl: AlertController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.cards = navParams.get('cards');
 
@@ -79,7 +44,8 @@ export class CarteraTarjetasPage {
    /** Añade una tarjeta de credito/debito a el cliente que se ha authenticado
    */
   addCard(){
-    this.storage.get('userId').then(value=>{
+    this.api.verifyUser().then((userInfo : any)=>{
+      let value = userInfo["CEDULA"]
       this.api.addCard(value).then((result) => {
         if (result){
           this.api.getAllCards(value).then((data: Array< {holder_name: String, expiry_year: String, expiry_month: String, icon: String, number: String}>) => {
